22e9950b39a27d6aaf2dc8ff06a920d715e12ff0
karanVR <karanveerraghuvanshi@gmail.com>
2024-08-17 19:34:21 +0530

    feat: initial setup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>
+        <text className='font-bold text-xl'> Login Page</text>
+     
+    </div>
+  )
+}
+
+export default page
